52df39c370af7ee3c5730b61db1424148f2cb6de
Gary Kuo <gary19910201@gmail.com>
2020-08-27 10:04:39 +0800

    Add count number

--- review-analytics.py
+++ review-analytics.py
@@ -21,3 +21,42 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
+
+# print(data[0])
+
+# 文字計數
+
+wc = {} # word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else: 
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+ 
+
+while True:
+    word = input('Type the word: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+    	print('沒有出現過喔')
+
+print('Thanks for using this tool')
